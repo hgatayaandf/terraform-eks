d1a575c6ec389d79adcbb4ab6aefdbcb583f2a16
hgatayaandf <121156667+hgatayaandf@users.noreply.github.com>
2024-07-30 19:09:43 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "devops-eks-${random_string.suffix.result}"
+  cluster_name = "demo-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
